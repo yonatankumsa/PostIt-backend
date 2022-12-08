6e41af0a80bc2b6abf2c43b64fbe7a24cd5a0c5d
yonatankumsa <guitaristyona@gmail.com>
2022-12-08 13:07:14 -0500

    yo

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ dotenv.config();
 const httpServer = require("http").createServer(app);
 const io = require("socket.io")(httpServer, {
   cors: {
-    origin: ["http://localhost:3000", "https://post-it-yo.onrender.com"],
+    origin: ["https://post-it-yo.onrender.com", "https://post-it-yo.onrender.com"],
   },
 });
 
